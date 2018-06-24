8710a0e93e7d0b42b920f0797add41ca7d420a3d
金花世界 <947575519@qq.com>
2018-06-24 20:56:04 +0800

    1：start，server成功；config配置目录不能带点，只有斜杠；开启局域网路由端口

---
+++ chat项目所有依赖项.js
@@ -0,0 +1,34 @@
+cnpm install webpack webpack-cli webpack-dev-server babel-core babel-loader babel-preset-env babel-preset-react react react-dom style-loader css-loader html-webpack-plugin  --save-dev
+cnpm install express mongoose body-parser cookie-parser socket.io nodemon socket.io-client axios  --save-dev
+---插件部分
+cnpm install clean-webpack-plugin  --save-dev  
+
+
+目录结构：
+
+chatonline
+		node_modules+ 依赖项
+		src+        自己写的静态文件目录
+			redux+       中间件
+			component+   辅助插件
+			container+   主路由文件
+			index.js    主入口js文件   需要修改配置文件的入口和出口路径了
+		server+     服务端的文件
+		build+      打包后静态资源存放的文件夹
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
